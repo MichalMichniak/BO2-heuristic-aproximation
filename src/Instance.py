af25aa2e08b240a767aaa2523515cf5aa9eb98db
MichalMichniak <michalmichniak2001@gmail.com>
2022-11-05 14:42:46 +0100

    funct vect documentation

--- src/Instance.py
+++ src/Instance.py
@@ -12,14 +12,31 @@ class Instance:
             architecture : List[int] - list of numbers of functions in each layer
             lut : LUT - look up table, dictionary which is mapping indexes -> functions
         """
+        ## meaning of funct_vect architecture description:
+        # first list (no. of layer)
+        # second list (no. of function in layer)
+        # tuple (index of function in LUT, index of 1st parameter [from previous layer], optional: index of 2nd parameter [from previous layer])
         self.funct_vect : List[List[Tuple[int]]] = [[]]
+
         self.lut : LUT = lut
         self.architecture = architecture
         self.t_max = t_max
         self.N = N
 
     def set_funct_vect(self, funct_vect : List[List[Tuple[int]]])->None:
+        """
+        set funct_vect.
+
+        args:
+            funct_vect : List[List[Tuple[int]]] - new function vector
+        """
         self.funct_vect = funct_vect
 
     def get_funct_vect(self)->List[List[Tuple[int]]]:
+        """
+        get funct_vect.
+
+        return:
+            funct_vect : List[List[Tuple[int]]] - function vector
+        """
         return self.funct_vect
