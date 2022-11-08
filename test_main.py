13c72c42c1da47f7e3ec3167b29d2bbfeceeb7c7
MichalMichniak <michalmichniak2001@gmail.com>
2022-11-08 20:18:42 +0100

    kk

--- test_main.py
+++ test_main.py
@@ -1,8 +1,8 @@
 import src.main as s
-import nothing_is_here.criterial_function as c
+import src.nothing_is_here.criterial_function as c
 import src.Objective_Function as l
 from copy import deepcopy
-
+import timeit
 if __name__ == '__main__':
     lut = s.LUT()
     f = lambda x,y: x+y
@@ -15,14 +15,17 @@ if __name__ == '__main__':
     lut.add_funct(f,2)
     lut.add_funct(f,5)
     lut.add_funct(f,1)
-    asc = s.ASC(lut,30,4,[4,4,4,4,2,1])
+    asc = s.ASC(lut,30,3,[4,4,4,4,2,1])
     
-    counter = c.Criterial_Funct_Counter(asc.architecture,asc.N,lut)
+    counter = c.Criterial_Funct_Counter(asc.architecture,asc.N,lut,nr_of_samplings_in_row=100)
     ista = asc.generate_instance()
     metric = lambda x,y: (x-y)**2
-    of = l.Objective_Function(s.prune(deepcopy(ista.get_funct_vect())),lut,metric)
+    of = l.Objective_Function(deepcopy(ista.get_funct_vect()),lut,metric)
     try:
-        print(of.calculate_for_all(asc.N,10,lambda x,y,z,w: 0))
-        print(counter.count_criterial_funct(ista.get_funct_vect(), lambda x,y,z,w: 0))
+        print("start")
+        print(of.calculate_for_all(asc.N,100,lambda x,y,z: 0))
+        print(counter.count_criterial_funct(ista.get_funct_vect(), lambda x,y,z: 0))
+        
+        
     except:
         print(ista.get_funct_vect())
