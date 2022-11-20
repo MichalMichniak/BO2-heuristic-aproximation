cf54ff687ef362ccc2db4e9075ade774eacf9ead
MichalMichniak <michalmichniak2001@gmail.com>
2022-11-20 21:56:10 +0100

    tak

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ import src.main_process as m
 
 
 if __name__ == '__main__':
-    m.main_process(20)
+    m.main_process(20,200)
+    
+    """ Pawel jak nie wierzysz w multiprocesing to to pod spodem robi to samo co na gorze"""
+    # funcs = s.get_funct()
+    # approx_func = s.get_approx_funct()
+    # lut = s.LUT()
+    # for i in funcs:
+    #     lut.add_funct(*i)
+    # asc = s.ASC(lut,140,2,[16,16,16,16,8,4,2,1])
+    # counter = s.Criterial_Funct_Counter(asc.architecture,asc.N,lut,nr_of_samplings_in_row=20)
+    # for i in range(200):
+    #     ista = asc.generate_instance()
+    #     print(counter.count_criterial_funct(ista.get_funct_vect(), approx_func))
     pass
--- src/main_process.py
+++ src/main_process.py
@@ -23,7 +23,7 @@ class Process_Package:
         self.conn1,self.conn2 = m.Pipe()
         self.thrr = threading.Thread(target=print_conn, args=[self.conn2,global_queue])
         self.buffor = m.Queue()
-        self.p = m.Process(target=slave_process, args=(self.conn1,self.buffor,architecture,N),kwargs={"nr_of_samplings_in_row":50,"process_id":1})
+        self.p = m.Process(target=slave_process, args=(self.conn1,self.buffor,architecture,N),kwargs={"nr_of_samplings_in_row":20,"process_id":1})
         pass
 
     def run(self):
@@ -45,7 +45,7 @@ def main_process(process_number = 3, instance_count = 100):
     asc = ASC(lut,140,2,[16,16,16,16,8,4,2,1])
     global_queue = queue.Queue()
     proc_lst = []
-    # p = m.Process(target=slave_process, args=(conn1,buffor,asc.architecture,asc.N),kwargs={"nr_of_samplings_in_row":100,"process_id":1})
+    maping_dict = {}
     ista = asc.generate_instance()
     for i in range(process_number):
         proc_lst.append(Process_Package(global_queue, asc.architecture,asc.N))
@@ -61,6 +61,7 @@ def main_process(process_number = 3, instance_count = 100):
             instance_count -=1
             ista = asc.generate_instance()
             temp_index = next(index)
+            maping_dict[temp_index] = temp_index
             print(temp_index)
             proc_lst[i].put([ista.get_funct_vect(),temp_index])
             
